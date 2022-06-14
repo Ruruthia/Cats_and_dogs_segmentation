f3d7a9a99e12a2788f0a4dcb0fb68a6c80201a0c
Maria Wyrzykowska <maria@pop-os.localdomain>
2022-06-14 14:24:15 +0200

    Add evaluate pipeline

--- src/cats_and_dogs_segmentation/pipeline_registry.py
+++ src/cats_and_dogs_segmentation/pipeline_registry.py
@@ -1,8 +1,9 @@
 """Project pipelines."""
 from typing import Dict
 
-from kedro.pipeline import Pipeline, pipeline
-from .pipelines import data_processing
+from kedro.pipeline import Pipeline
+
+from .pipelines import data_processing, train_model, evaluate_model
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -12,6 +13,11 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     data_processing_pipeline = data_processing.create_pipeline()
-    return {"__default__": pipeline([data_processing_pipeline]),
+    train_model_pipeline = train_model.create_pipeline()
+    evaluate_model_pipeline = evaluate_model.create_pipeline()
+    evaluate_model_independent_pipeline = evaluate_model.create_pipeline_independent()
+    return {"__default__": data_processing_pipeline + train_model_pipeline + evaluate_model_pipeline,
             "data_processing": data_processing_pipeline,
+            "train_model": data_processing_pipeline + train_model_pipeline + evaluate_model_pipeline,
+            "evaluate_model": evaluate_model_independent_pipeline
             }
--- src/cats_and_dogs_segmentation/pipelines/train_model/pipeline.py
+++ src/cats_and_dogs_segmentation/pipelines/train_model/pipeline.py
@@ -4,7 +4,16 @@ generated using Kedro 0.18.1
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import train
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=train,
+            inputs=["train_data_loader", "val_data_loader", "params:config",
+                    "params:num_epochs", "params:gpus", "params:project"],
+            outputs="model_path",
+            name="train_model"
+        )
+    ])
