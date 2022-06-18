0659a28cc82da8b01f4b5b5702dec9c34c09851b
Maria Wyrzykowska <maria@pop-os.localdomain>
2022-06-18 17:53:30 +0200

    Checkout to temporary branch in startup script

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+cd ~
 git clone https://$(gcloud secrets versions access latest --secret github-pat)@github.com/Ruruthia/Cats_and_dogs_segmentation.git
 cd Cats_and_dogs_segmentation
-conda env create -f src/environment.yml -n cads
-conda activate cads
-wandb login $(gcloud secrets versions access latest --secret wandb)
+git checkout 6-terraform-provisioning
