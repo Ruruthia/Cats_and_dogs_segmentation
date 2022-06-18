7e1ca2e6ba929b6bc6d424f8f2bb21097bd541a3
Maria Wyrzykowska <maria@pop-os.localdomain>
2022-06-18 14:26:56 +0200

    WIP terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "4.25.0"
     }
   }
 }
@@ -16,9 +16,36 @@ provider "google" {
 }
 
 resource "google_compute_disk" "default" {
-  name  = "cads-persistent-disc"
-  type  = "pd-balanced"
-  zone  = "europe-central2-a"
+  name = "cads-persistent-disk"
+  type = "pd-balanced"
+  zone = "europe-central2-a"
   size = 100
+}
+
+resource "google_compute_instance" "default" {
+  name                    = "cads-instance-1"
+  machine_type            = "e2-standard-2"
+  zone                    = "europe-central2-a"
+  metadata_startup_script = file("${path.module}/startup.sh")
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ml-images/global/images/c2-deeplearning-pytorch-1-11-xla-v20220316-debian-10"
+    }
+  }
+
+  attached_disk {
+    source      = google_compute_disk.default.self_link
+    mode        = "READ_WRITE"
+    device_name = "cads-disk"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+
+    }
+  }
 }
 
